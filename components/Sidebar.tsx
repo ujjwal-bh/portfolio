fa829002f46c398ccfeb6a9829c01814f2a4e197
ujjwal-bh <ujjwalbh001@gmail.com>
2024-04-30 23:58:51 +0530

    sidebar button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,120 +1,123 @@
-"use client"
-import { data } from '@/utils/data'
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { FaCalendar, FaChevronDown, FaEnvelope, FaFacebook, FaInstagram, FaPhoneAlt, FaTwitter } from 'react-icons/fa'
-import { FaLocationPin } from 'react-icons/fa6'
+"use client";
+import { data } from "@/utils/data";
+import Image from "next/image";
+import React, { useState } from "react";
+import {
+  FaCalendar,
+  FaChevronDown,
+  FaChevronUp,
+  FaEnvelope,
+  FaFacebook,
+  FaInstagram,
+  FaPhoneAlt,
+  FaTwitter,
+} from "react-icons/fa";
+import { FaLocationPin } from "react-icons/fa6";
 
 export default function Sidebar() {
   const [active, setActive] = useState(false);
   return (
-    <aside className={`sidebar ${active? "active": ""}`}>
+    <aside className={`sidebar ${active ? "active" : ""}`}>
+      <div className="sidebar-info">
+        <figure className="avatar-box">
+          <Image
+            src="/img/my-avatar.png"
+            alt="Richard hanrick"
+            width={80}
+            height={100}
+          />
+        </figure>
 
-    <div className="sidebar-info">
+        <div className="info-content">
+          <h1 className="name" title="Richard hanrick">
+            {data.name}
+          </h1>
 
-      <figure className="avatar-box">
-        <Image src="/img/my-avatar.png" alt="Richard hanrick" width={80} height={100}/>
-      </figure>
+          <p className="title">{data.tagline}</p>
+        </div>
 
-      <div className="info-content">
-        <h1 className="name" title="Richard hanrick">{data.name}</h1>
+        <button
+          className={`info_more-btn`}
+          onClick={() => setActive((prev) => !prev)}
+        >
+          <span>Show Contacts</span>
 
-        <p className="title">{data.tagline}</p>
+          {active ? <FaChevronUp /> : <FaChevronDown />}
+        </button>
       </div>
 
-      <button className={`info_more-btn`} onClick={()=> setActive(prev => !prev)}>
-        <span>Show Contacts</span>
+      <div className={`sidebar-info_more ${active && "active"}`}>
+        <div className="separator"></div>
 
-        <FaChevronDown />
-      </button>
+        <ul className="contacts-list">
+          <li className="contact-item">
+            <div className="icon-box">
+              <FaEnvelope />
+            </div>
 
-    </div>
+            <div className="contact-info">
+              <p className="contact-title">Email</p>
 
-    <div className={`sidebar-info_more ${active && "active"}`}>
+              <a href={`mailto:${data.email}`} className="contact-link">
+                {data.email}
+              </a>
+            </div>
+          </li>
 
-      <div className="separator"></div>
+          <li className="contact-item">
+            <div className="icon-box">
+              <FaPhoneAlt />
+            </div>
 
-      <ul className="contacts-list">
+            <div className="contact-info">
+              <p className="contact-title">Phone</p>
 
-        <li className="contact-item">
+              <a href={`tel:${data.phone}`} className="contact-link">
+                {data.phone}
+              </a>
+            </div>
+          </li>
 
-          <div className="icon-box">
-            <FaEnvelope/>
-          </div>
+          <li className="contact-item">
+            <div className="icon-box">
+              <FaCalendar />
+            </div>
 
-          <div className="contact-info">
-            <p className="contact-title">Email</p>
+            <div className="contact-info">
+              <p className="contact-title">Birthday</p>
 
-            <a href={`mailto:${data.email}`} className="contact-link">{data.email}</a>
-          </div>
+              <time dateTime={data.birthdate}>{data.birthdate}</time>
+            </div>
+          </li>
 
-        </li>
+          <li className="contact-item">
+            <div className="icon-box">
+              <FaLocationPin />
+            </div>
 
-        <li className="contact-item">
+            <div className="contact-info">
+              <p className="contact-title">Location</p>
 
-          <div className="icon-box">
-            <FaPhoneAlt/>
-          </div>
+              <address>{data.location}</address>
+            </div>
+          </li>
+        </ul>
 
-          <div className="contact-info">
-            <p className="contact-title">Phone</p>
+        <div className="separator"></div>
 
-            <a href={`tel:${data.phone}`} className="contact-link">{data.phone}</a>
-          </div>
-
-        </li>
-
-        <li className="contact-item">
-
-          <div className="icon-box">
-            <FaCalendar/>
-          </div>
-
-          <div className="contact-info">
-            <p className="contact-title">Birthday</p>
-
-            <time dateTime={data.birthdate}>{data.birthdate}</time>
-          </div>
-
-        </li>
-
-        <li className="contact-item">
-
-          <div className="icon-box">
-            <FaLocationPin/>
-          </div>
-
-          <div className="contact-info">
-            <p className="contact-title">Location</p>
-
-            <address>{data.location}</address>
-          </div>
-
-        </li>
-
-      </ul>
-
-      <div className="separator"></div>
-
-      <ul className="social-list">
-        {
-          data.socialMedia.map((item, index)=> {
-            const {Icon, url} = item
+        <ul className="social-list">
+          {data.socialMedia.map((item, index) => {
+            const { Icon, url } = item;
             return (
               <li className="social-item" key={index}>
-                <a href={url} target="_blank" className='social-link'>
-                  <Icon/>
+                <a href={url} target="_blank" className="social-link">
+                  <Icon />
                 </a>
               </li>
-
-            )
-          })
-        }
-
-      </ul>
-
-    </div>
-
-  </aside>
-  )
+            );
+          })}
+        </ul>
+      </div>
+    </aside>
+  );
 }
